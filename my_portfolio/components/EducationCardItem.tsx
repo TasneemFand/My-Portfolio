59d8e7a5e6901cbcfdd397d9dcd836b1feb8cd50
Tasneem Fandakli <tasnimtepe@gmail.com>
2023-10-09 23:45:59 +0300

    add education section

---
+++ my_portfolio/components/EducationCardItem.tsx
@@ -0,0 +1,45 @@
+import { TEducation } from "@/data";
+import Image from "next/image";
+
+export const EducationCardItem = ({ info }: { info: TEducation[0] }) => {
+  return (
+    <div
+      className="w-[650px] border-[0.1px] border-solid border-[#854CE6] rounded-2xl
+        shadow-[0px_4px_24px_rgba(23,92,230,0.15)] py-3 px-4 flex flex-col gap-3
+        transition ease-in-out duration-[0.3s] hover:shadow-[0px_0px_20px_rgba(0,0,0,0.2)] hover:translate-y-[-5px] relative overflow-hidden
+        max-[768px]:p-3 max-[768px]:gap-2 max-[768px]:w-[300px] bg-[#1c1c27]"
+    >
+      <div className="w-full flex gap-3">
+        {info.img ? (
+          <Image
+            src={info?.img}
+            alt={info.faculty}
+            width={50}
+            height={50}
+            className="self-start rounded-md"
+          />
+        ) : null}
+        <div className="flex flex-col gap-1">
+          <div className="text-lg font-bold text-[#f2f3f499] max-[768px]:text-sm ">
+            {info.faculty}, {info.universityName} University
+          </div>
+          <div className="text-sm font-semibold text-[#b1b2b399] max-[768px]:text-xs">
+            {info.degree} of {info.faculty} - {info.specialization}
+          </div>
+          <div className="text-xs max-[768px]:text-[10px] font-semibold text-[#b1b2b380]">
+            {info.startData} - {info.endDate}
+          </div>
+        </div>
+      </div>
+      <div className="px-3 flex flex-col gap-2">
+        <div className="text-sm font-semibold text-[#b1b2b399] max-[768px]:text-xs ">
+          <b>Grade: </b>
+          {info.grade}
+        </div>
+        <div className="text-sm font-semibold text-[#f2f3f499]  max-[768px]:text-xs leading-7 flex flex-wrap">
+          <span>{info.descrp}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
--- my_portfolio/data.tsx
+++ my_portfolio/data.tsx
@@ -3,6 +3,31 @@ export type TSkills = {
   img: string;
   width: number;
 }[];
+export type TEducation = {
+  universityName: string;
+  faculty: string;
+  degree: string;
+  startData: string;
+  endDate: string;
+  grade: String;
+  img?: string;
+  specialization: string;
+  descrp: string;
+}[];
+
+export type TExperience = {
+  companyName: string;
+  position: string;
+  startData: string;
+  endDate: string;
+  skills: string[];
+  descrp: string;
+  img: string;
+  worktype: string;
+  location: string;
+  tasks: string[];
+  link: string;
+}[]
 
 export const skills: TSkills = [
   {
@@ -96,3 +121,57 @@ export const skills: TSkills = [
     width: 100,
   },
 ];
+
+export const education:TEducation = [
+  {
+    universityName: "Damascus",
+    faculty: "Information Technology Engineering",
+    degree: "Bachelor",
+    startData: "2016",
+    endDate: "2021",
+    grade: "76.23%",
+    img: "/images/ITE.jpg",
+    specialization: "AI",
+    descrp: "I graduated with a bachelor's degree in information technology engineering at Damascus university, syria. I completed 10 semesters and have a degree of 76.23% and a CGPA of 8.02. I taken courses in Data Structures, Algorithms, Object-Oriented Programming languages (C++,Java), Database (MySQL), Operating Systems, Computer Networks, Mathematics and Statistics, AI (machine learning, deep learning, natural language processing). I worked on various group projects in my college."
+
+  }
+]
+
+export const experience:TExperience = [
+  {
+    companyName: 'EcoMundo',
+    position: 'Frontend Web Developer',
+    startData: 'Feb 2022',
+    endDate: 'Oct 2023',
+    worktype: 'Remotely',
+    location: 'Issy-les-Moulineaux, Île-de-France, France',
+    img: '/images/EcoMundo.jpg',
+    descrp: 'A Firm contains 51-200 employees around the world that provides expert services and software tools to their clients in the areas of Chemicals/Toxicology and Regulations.',
+    link: 'https://en.ecomundo.eu',
+    tasks: [
+      'Discussed and reviewed specifications and mockups with the development team.',
+      'Developed and implemented highly responsive user interfaces using React.js and Ext.js framework.',
+      'Worked on migrating applications user interfaces from Ext.js framework to React.js library.',
+      'Played a role in Code review and refactoring the code.',
+      'Debugged and fixed bugs of the applications.',
+      'Presented product demos to the client.'
+    ],
+    skills: [
+      'HTML',
+      'CSS',
+      'JavaScript',
+      'TypeScript',
+      'React JS',
+      'Ext JS',
+      'React Query',
+      'Recoil',
+      'React Router',
+      'Material UI',
+      'Graph QL',
+      'Agile',
+      'Git',
+      'Gitlab'
+    ]
+   
+  }
+]
