727da0fc2dd4d45937b90848d83f194d974a3134
Tasneem Fandakli <tasnimtepe@gmail.com>
2023-10-17 12:47:44 +0300

    fix

--- components/header.tsx
+++ components/header.tsx
@@ -14,6 +14,7 @@ export const Header = () => {
             alt={"logo"}
             width={200}
             height={200}
+            priority
           />
           {/**button */}
           <Link
