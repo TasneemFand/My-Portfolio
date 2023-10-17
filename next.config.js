155a0bb8d323e89ee737cbf8411f6a0b381ff9d2
Tasneem Fandakli <tasnimtepe@gmail.com>
2023-10-17 11:54:04 +0300

    next config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@
 const nextConfig = {
     /* config options here */
     output: 'export',
+    env: {
+      NEXT_PUBLIC_SERVICE_ID: 'service_cst003s',
+      NEXT_PUBLIC_TEMPLATE_ID:'template_vt88k93',
+      NEXT_PUBLIC_KEY: 'X2nw_bo82E1Rd8t7b'
+
+    }
   }
-   
+  
   module.exports = nextConfig
