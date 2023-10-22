4f6354cb53ce2b9bbfc16ff4995c9a0a9c21fd92
Tasneem Fandakli <tasnimtepe@gmail.com>
2023-10-22 17:13:25 +0300

    deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import './globals.css'
 import type { Metadata } from 'next'
+import { Analytics } from '@vercel/analytics/react';
 
 
 
@@ -25,6 +26,7 @@ export default function RootLayout({
             >
               {children}
           </ThemeProvider>
+          <Analytics />
       </body>
     </html>
   )
